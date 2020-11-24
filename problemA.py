1292f5042aaf548d01fb70c6b0feb3c76f58b0c8
Cameron Lamont <cameron.lamont1@ucalgary.ca>
2020-11-24 10:53:39 -0700

    more sh!t

--- problemA.py
+++ problemA.py
@@ -1,12 +1,23 @@
 import csv
+import pandas as pd
 
-def pressuresurgecalc():
+def pressuresurgenumb():
     nps = input("What is the nominal pipe size?")
 
-    with open("PipeData.csv") as PipeData:
-        csv_reader = csv.reader(PipeData)
+    data = pd.read_csv("PipeData.csv", header=None, delimiter=r"\s+")
 
-        for row in csv_reader:
-            if nps == row[0]:
-                Do = row[1]
+    for row in data:
+        if row[0] == nps:
+            Do = row[1]
+            pipsched = row[2]
+            t = row[3]
+            
+            if type(pipsched) != type(1):
+                pipsched = float(input("A pipe schedule number was not valid, please input a pipe schedule number."))
+        
+        else:
+            print("The nominal pipe size was not found.")
+
+    data1 = pd.read_csv("MaterialData.csv", header=None, delimiter=r"\s+")
+
     
