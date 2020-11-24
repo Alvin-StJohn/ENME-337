33b814049e5878e00d623776d59dc55e25efe072
Cameron Lamont <cameron.lamont1@ucalgary.ca>
2020-11-24 11:09:39 -0700

    yeah

--- problemA.py
+++ problemA.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 def pressuresurgenumb():
     nps = input("What is the nominal pipe size?")
-
+    pipsched = input("What is the pipe schedule?")
+    pipmat = input("What is the pipe material?")
     data = pd.read_csv("PipeData.csv", header=None, delimiter=r"\s+")
 
     for row in data:
-        if row[0] == nps:
-            Do = row[1]
-            pipsched = row[2]
-            t = row[3]
+        if row[0] == nps and row[2] == pipsched:
+            Do1 = row[1]
+            t1 = row[3]
             
             if type(pipsched) != type(1):
                 pipsched = float(input("A pipe schedule number was not valid, please input a pipe schedule number."))
